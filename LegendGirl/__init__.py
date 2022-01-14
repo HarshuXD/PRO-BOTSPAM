d975750747f3e5de6c26a832a0a462d5cb6f71db
PROBOY-OP <pro45legendboy@gmail.com>
2022-01-14 16:11:45 +0000

    auto-fixes

--- LegendGirl/__init__.py
+++ LegendGirl/__init__.py
@@ -1,11 +1,13 @@
-from telethon import TelegramClient
-from decouple import config
 import logging
 import time
 from os import getenv
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+from decouple import config
+from telethon import TelegramClient
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
 
 # Basics
 APP_ID = config("APP_ID", default=None, cast=int)
@@ -24,16 +26,16 @@ BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
-bot = TelegramClient('LegendBoy', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
-bot2 = TelegramClient('LegendBoy2', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
-bot3 = TelegramClient('LegendBoy3', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
-bot4 = TelegramClient('LegendBoy4', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
-bot5 = TelegramClient('LegendBoy5', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
-bot6 = TelegramClient('LegendBoy6', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
-bot7 = TelegramClient('LegendBoy7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
-bot8 = TelegramClient('LegendBoy8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
-bot9 = TelegramClient('LegendBoy9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
-bot10 = TelegramClient('LegendBoy10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+bot = TelegramClient("LegendBoy", APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
+bot2 = TelegramClient("LegendBoy2", APP_ID, API_HASH).start(bot_token=BOT_TOKEN2)
+bot3 = TelegramClient("LegendBoy3", APP_ID, API_HASH).start(bot_token=BOT_TOKEN3)
+bot4 = TelegramClient("LegendBoy4", APP_ID, API_HASH).start(bot_token=BOT_TOKEN4)
+bot5 = TelegramClient("LegendBoy5", APP_ID, API_HASH).start(bot_token=BOT_TOKEN5)
+bot6 = TelegramClient("LegendBoy6", APP_ID, API_HASH).start(bot_token=BOT_TOKEN6)
+bot7 = TelegramClient("LegendBoy7", APP_ID, API_HASH).start(bot_token=BOT_TOKEN7)
+bot8 = TelegramClient("LegendBoy8", APP_ID, API_HASH).start(bot_token=BOT_TOKEN8)
+bot9 = TelegramClient("LegendBoy9", APP_ID, API_HASH).start(bot_token=BOT_TOKEN9)
+bot10 = TelegramClient("LegendBoy10", APP_ID, API_HASH).start(bot_token=BOT_TOKEN10)
 
 
 RAID = [
